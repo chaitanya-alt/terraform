9e55027bc898a4fb1b621eb4ed84c73fd907e7bf
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-15 04:52:48 +0530

    terraform

--- expense/r53.tf
+++ expense/r53.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "expense" {
     count = length(var.instance_name)
     type = "A"
     ttl = 1
-    name = "${var.instance_names}"."${var.domain_name}"                #backend.online.com
+    name = "${var.instance_names}".${var.domain_name}"                #backend.online.com
     records = [aws_instance.expense[count.index].private_ip]              #ip
     allow_overwrite = true
 
