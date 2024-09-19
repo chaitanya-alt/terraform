c2734ac8edcac372eda4b80b2737e95f3e423506
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-19 12:38:44 +0530

    locals

--- locals/r53.tf
+++ locals/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record"  "terraform" {
     count = length(var.instance_names)
     name = var.instance_names[count.index] == "frontend" ? local.domain_name : "${var.instance_names[count.index]}.${local.domain_name}"
-    records = var.instance_names == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terraform[count.index].private_ip]
+    records = var.instance_names[count.index] == "frontend" ? [aws_instance.terraform[count.index].public_ip] : [aws_instance.terraform[count.index].private_ip]
     type = "A"
     zone_id = local.zone_id
     ttl = 1
