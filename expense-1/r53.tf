a33eab8e664f8e949a720bfddad9e00b44361d1d
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-22 05:08:28 +0530

    expense

--- expense-1/r53.tf
+++ expense-1/r53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "expense" {
     count = length(var.instance_names)
     zone_id = "Z09903562VXZQBZB5OUJC"
     name = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"              #frontend.chaitanyaproject.online
-    records = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[coount.index].public_ip]
+    records = var.instance_names[count.index] == "frontend" ? [aws_instance.expense[count.index].public_ip] : [aws_instance.expense[count.index].private_ip]
     type = "A"
     ttl = 1
     allow_overwrite = true
