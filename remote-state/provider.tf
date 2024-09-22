628f836cc1c46a02561f533606906f62a62b6011
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-09-22 06:44:48 +0530

    remote-state

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -1,12 +1,15 @@
 terraform {
     required_providers {
+      aws ={
       source = "hashicorp/aws"
       version = "5.66.0"
+    }
     }
     backend "s3" {
         bucket = "chaitanya-s3-306"
         key = "remote-state-file"
         dynamodb_table = "chaitanya-306"
+        region = "us-east-1"
     }
 }
 
